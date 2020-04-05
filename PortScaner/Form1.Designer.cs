c009d7974988731066fb0a6f04c4fb6263a655de
davidxxie <xrf_mail@163.com>
2020-04-05 17:43:57 +0800

    add PortScaner

---
+++ PortScaner/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PortScaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPortRange = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(454, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 83);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(490, 257);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Host";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(72, 38);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(140, 21);
+            this.txtHost.TabIndex = 3;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PortRange";
+            // 
+            // txtPortRange
+            // 
+            this.txtPortRange.Location = new System.Drawing.Point(315, 38);
+            this.txtPortRange.Name = "txtPortRange";
+            this.txtPortRange.Size = new System.Drawing.Size(100, 21);
+            this.txtPortRange.TabIndex = 5;
+            this.txtPortRange.Text = "8000-8005";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 366);
+            this.Controls.Add(this.txtPortRange);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PortScaner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPortRange;
+    }
+}
+
---
+++ PortScaner/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace PortScaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
+            button1.Enabled = false;
+            Program.Scan(this.textBox1, txtHost.Text, txtPortRange.Text, button1);
+        }
+
+        public class Program
+        {
+            //已扫描端口数目
+            internal static int scannedCount = 0;
+
+            internal static int runningThreadCount = 0;
+
+            internal static List<int> openedPorts = new List<int>();
+            internal static List<int> closedPorts = new List<int>();
+
+            static int startPort = 1;
+            static int endPort = 500;
+
+            static int maxThread = 100;
+
+
+            public static void Scan(System.Windows.Forms.TextBox aTextBox, string aHost, string aPortRange, Button aButton)
+            {
+                openedPorts.Clear();
+                closedPorts.Clear();
+                scannedCount = 0;
+                runningThreadCount = 0;
+                startPort = 0;
+                endPort = 0;
+
+                string host = aHost;
+                string portRange = aPortRange;
+                startPort = int.Parse(portRange.Split('-')[0].Trim());
+                endPort = int.Parse(portRange.Split('-')[1].Trim());
+
+                for (int port = startPort; port < endPort; port++)
+                {
+                    Scanner scanner = new Scanner(host, port);
+                    Thread thread = new Thread(new ThreadStart(scanner.Scan));
+                    thread.Name = port.ToString();
+                    thread.IsBackground = true;
+                    thread.Start();
+
+                    runningThreadCount++;
+                    Thread.Sleep(10);
+
+                    //循环，直到某个线程工作完毕才启动另一新线程，也可以叫做推拉窗技术
+                    while (runningThreadCount >= maxThread) ;
+                }
+                //空循环，直到所有端口扫描完毕
+                while (scannedCount + 1 <= (endPort - startPort))
+                {
+                    Thread.Sleep(10);
+                    Console.WriteLine();
+                }
+                
+
+                //输出结果
+                aTextBox.Clear();
+                aTextBox.Text = "Scan for host:"+host+" has been completed, \r\n total "+(endPort - startPort)+" ports scanned, \n opened ports:"+openedPorts.Count;
+                foreach (int port in openedPorts)
+                {
+                    aTextBox.Text=aTextBox.Text+"\r\nport:"+port.ToString().PadLeft(6)+" is open";
+                }
+                foreach (int port in closedPorts)
+                {
+                    aTextBox.Text = aTextBox.Text + "\r\nport:" + port.ToString().PadLeft(6) + " is closed";
+                }
+                aButton.Enabled = true;
+            }
+        }
+
+        class Scanner
+        {
+            string m_host;
+            int m_port;
+
+            public Scanner(string host, int port)
+            {
+                m_host = host;
+                m_port = port;
+            }
+            public void Scan()
+            {
+                TcpClient tc = new TcpClient();
+                tc.SendTimeout = tc.ReceiveTimeout = 2000;
+
+                try
+                {
+                    tc.Connect(m_host, m_port);
+                    if (tc.Connected)
+                    {
+                        Program.openedPorts.Add(m_port);
+                    }
+                }
+                catch
+                {
+                    Program.closedPorts.Add(m_port);
+                }
+                finally
+                {
+                    tc.Close();
+                    tc = null;
+                    Program.scannedCount++;
+                    Program.runningThreadCount--;
+                }
+            }
+        }
+    }
+}
